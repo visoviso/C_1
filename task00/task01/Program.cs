3976c9a388f319e952bfab52f7f0f629acf5a37b
Violetta Sorokina <violetochka@hotmail.com>
2022-12-15 21:47:27 +0300

    1

---
+++ task00/task01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine("да");
+}
+else
+    Console.WriteLine("нет");
