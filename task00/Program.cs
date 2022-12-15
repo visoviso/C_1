ea3be682eddb76cc8d73ea85766bc9885b37c9b1
Violetta Sorokina <violetochka@hotmail.com>
2022-12-15 21:13:31 +0300

    0

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Напишите программу  которая 
+// 1.на вход ( данные вводит пользователь) принимает число и 
+// 2.выдает его квадрат ( 4-16 3-9 7-49)
+
+Console.WriteLine("введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"квадрат числа {number} = {square}");
+
